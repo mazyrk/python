cae2e62e7bb61769f1dda5560c4f74c40dcf8e2f
mainak-wq <mainakdeb1998@gmail.com>
2022-10-16 12:30:50 +0530

    16th October

--- dictionary and numpy.py
+++ dictionary and numpy.py
@@ -48,19 +48,89 @@
 
 
 
-dict_1={
-    "name":"Mainak",
-    "age":"33",
-    "roll":9999,
-    "city":"Kolkata",
-    "Subject":"Python",
-    "Year":2000,
-    # "Year":1900, 
-}
-print(type(dict_1))
+# dict_1={
+#     "name":"Mainak",
+#     "age":"33",
+#     "roll":9999,
+#     "city":"Kolkata",
+#     "Subject":"Python",
+#     "Year":2000,
+#     # "Year":1900, 
+# }
+# print(type(dict_1))
+# print("\n")
+# for x,y in dict_1.items():
+#     print(x,":",y)
+
+
+
+
+
+
+
+# test_2={
+#     "a":50,
+#     "b":60,
+#     "c":100,
+# }
+# x=test_2["a"]+test_2["b"]+test_2["c"]
+# print(x)
+
+
+
+
+
+#NUMPY ARRAY MERGING
+
+# import numpy as np
+# arr_1=np.array([2,3,6,5])
+# arr_2=np.array([20,3,6,64])
+# arr=np.concatenate((arr_1,arr_2))
+# print(arr)
+
+
+# import numpy as np
+# arr=np.array([1,3,6,16,1,61,56,1,222,5])
+# arr_1=np.array(5)
+# print(arr.ndim)
+# print(arr_1.ndim)
+# arr_2=np.array([[1,2,3],[5,6,7]])
+# print(arr_2, arr_2.ndim)
+
+
+
+
+
+# import numpy as np
+# arr_1=np.array([2,4,6,8,10,12])
+# arr_2=np.array([1,3,5,7,9,11])
+# x=arr_1[2]
+# y=arr_2[3]
+# print("Elements addition -> ",x+y)
+
+
+
+
+# import numpy as np
+# arr=np.array([1,2,3,4,5,6])
+# # y=arr[3]
+# arr_1=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+# z=arr_1[1,1]
+# print(z)
+
+
+
+import numpy as np
+arr=np.array([[[1,2,3,4],[5,6,7,8]],[[9,10,11,12],[13,14,15,16]]])
+arr_1=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+abc=arr[0,1,2]
+xyz=arr[1,1,1]
+print(abc)
+print(xyz)
 print("\n")
-for x,y in dict_1.items():
-    print(x,":",y)
+print(arr.ndim,"D"," ",arr.shape)
+print(arr_1.ndim,"D"," ",arr_1.shape)
+
 
 
 
---
+++ file_read_write.py
@@ -0,0 +1,65 @@
+# a=open("test.txt","r")
+# print(a.read())
+
+
+
+
+# a=open("test.txt","a")
+# # print(a.read())
+# a.write("\nHello I am student of MIT college")
+# a.close()
+# a=open("test.txt","r")
+# print(a.read())
+
+
+
+# a=open("test.txt","w")
+# a.write("We are learning Python Language")
+# a.close()
+# a=open("test.txt","r")
+# print(a.read())
+
+
+
+# # b=open("test_2.txt","x")
+# # print(b)
+
+
+
+# import os as abc
+# abc.remove("test_2.txt")
+
+
+
+#python program to create a new file, write and print
+
+# a=open("test.txt","x")   #creating a file
+a=open("test.txt","w")   #creating file and writing
+a.write("Hello we are learning Python Programming")
+a.close()
+a=open("test.txt","a")
+a.write("\nHello My name is Google")
+a.close()
+a=open("test.txt","r")
+print(a.read())
+
+
+
+# import os 
+# os.remove("test.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
