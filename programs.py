d337b3145055fd889f172c44fe2ded269bac1e4d
mainak-wq <mainakdeb1998@gmail.com>
2022-12-10 14:29:26 +0530

    mazy's python codes

---
+++ programs.py
@@ -0,0 +1,37 @@
+# a=10
+# b=25.33
+# c=23+45j
+# d="Hello World"
+# e=True
+# f=False
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+
+# t="mousuni"
+# print(type(t),t)
+
+
+# a=str(input("Enter your name -> "))
+# b=str(input("Enter your surname -> "))
+# print("Your name is -> ",a+" "+b)
+
+
+# class Input:
+#     def tithi():
+#         abc=int(input("Enter first number -> "))
+#         xyz=int(input("Enter second number -> "))
+#         result=abc+xyz
+#         result_1=abc-xyz
+#         result_2=abc*xyz
+#         result_3=abc/xyz
+#         print("Add -> ",result)
+#         print("Sub -> ",result_1)
+#         print("Mul -> ",result_2)
+#         print("Div -> ",result_3)
+# obj_test=Input
+# obj_test.tithi()
